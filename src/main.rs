6c472cd0378c59e588ed20be0e8fea63680174e6
Hqnnqh <hannah@diefluchs.at>
2024-12-25 22:27:22 +0100

    feat: set up writer

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@
 
 extern crate alloc;
 
-use core::panic::PanicInfo;
+use core::{fmt::Write, panic::PanicInfo};
 
+use framebuffer::{color::Color, raw::write::RawWriter};
 use graphics::{initialize_framebuffer, parse_psf_font};
 use log::{error, info};
 use qemu_print::qemu_println;
@@ -23,23 +24,17 @@ fn main() -> Status {
     info!("Bootloader: Intializing GOP framebuffer...");
     match initialize_framebuffer() {
         Ok(framebuffer) => {
-            for x in 0..framebuffer.width() {
-                for y in 0..framebuffer.height() {
-                    framebuffer
-                        .draw_pixel(
-                            x,
-                            y,
-                            framebuffer::color::Color {
-                                red: 0,
-                                green: 128,
-                                blue: 128,
-                            },
-                        )
-                        .unwrap();
-                }
-            }
+            framebuffer.fill(Color::new(0, 0, 0));
             let font = parse_psf_font(PSF_FILE_NAME).unwrap();
-            qemu_print::qemu_println!("font: {:#?}", font);
+
+            let mut writer = RawWriter::new(
+                font,
+                framebuffer,
+                Color::new(255, 255, 255),
+                Color::new(0, 0, 0),
+            );
+
+            writer.write_str("Writer works! :)").unwrap();
         }
         // this won't always be shown in the console, because stdout may not be avaialable in some cases
         Err(err) => error!("Bootloader: Failed to initialize framebuffer: {}", err),
